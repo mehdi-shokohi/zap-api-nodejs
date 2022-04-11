3edb1e32f9de91ca80aee3690da00b31840e46c2
mehdi <mehdi.shokohifar@gmail.com>
2022-04-12 03:27:27 +0430

    Replacer : add rule on Site

--- src/replacer.js
+++ src/replacer.js
@@ -59,6 +59,29 @@ Replacer.prototype.addRule = function (description, enabled, matchtype, matchreg
 };
 
 /**
+ * Adds a replacer rule . For the parameters: desc is a user friendly description, enabled is true or false, matchType is one of [REQ_HEADER, REQ_HEADER_STR, REQ_BODY_STR, RESP_HEADER, RESP_HEADER_STR, RESP_BODY_STR], matchRegex should be true if the matchString should be treated as a regex otherwise false, matchString is the string that will be matched against, replacement is the replacement string, initiators may be blank (for all initiators) or a comma separated list of integers as defined in <a href="https://github.com/zaproxy/zaproxy/blob/develop/src/org/parosproxy/paros/network/HttpSender.java">HttpSender</a>  
+ * This component is optional and therefore the API will only work if it is installed
+ **/
+
+
+Replacer.prototype.addRuleOnSite = function (description, enabled, matchtype, matchregex, matchstring, replacement, initiators,site, callback) {
+  const params = {'description' : description, 'enabled' : enabled, 'matchType' : matchtype, 'matchRegex' : matchregex, 'matchString' : matchstring};
+  if (replacement && replacement !== null) {
+    params['replacement'] = replacement;
+  }
+  if (initiators && initiators !== null) {
+    params['initiators'] = initiators;
+  }
+  if (site && site !== null) {
+    params['site'] = site;
+  }
+  if (typeof callback === 'function') {
+    this.api.request('/replacer/action/addRuleOnSite/', params, callback);
+    return;
+  }
+  return this.api.requestPromise('/replacer/action/addRuleOnSite/', params);
+};
+/**
  * Removes the rule with the given description
  * This component is optional and therefore the API will only work if it is installed
  **/
