49f01e94b2ab7b83bcb278f566763ac0b01be74a
mehdi <mehdi.shokohifar@gmail.com>
2022-06-01 16:48:25 +0430

    add alert api

---
+++ src/alert.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * This file was automatically generated.
+ */
+function Alert(clientApi) {
+  this.api = clientApi;
+}
+
+/**
+ * 
+ **/
+ Alert.prototype.alertSummary = function (url,recurse,callback) {
+     let params ={recurse:false}; 
+     if (url && url!==null && url !=="") {
+         params.url = url
+     }
+     if (recurse && url!==null ) {
+        params.recurse = url
+    }
+  if (typeof callback === 'function') {
+    this.api.request('/alert/view/alertCountsByRisk/',params ,callback);
+    return;
+  }
+  return this.api.requestPromise('/alert/view/alertCountsByRisk/',params);
+};
+
+Alert.prototype.alertsByRisk = function (url,recurse,callback) {
+    let params ={recurse:false}; 
+    if (url && url!==null && url !=="") {
+        params.url = url
+    }
+    if (recurse && url!==null ) {
+       params.recurse = url
+   }
+ if (typeof callback === 'function') {
+   this.api.request('/alert/view/alertsByRisk/',params ,callback);
+   return;
+ }
+ return this.api.requestPromise('/alert/view/alertsByRisk/',params);
+};
